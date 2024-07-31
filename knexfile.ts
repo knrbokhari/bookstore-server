425578425a765fffae8a7606fbd38bf0a41a30d2
knrbokhari <kazinaeemrayhan@gmail.com>
2024-07-31 13:18:46 +0600

    Database connected

---
+++ knexfile.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
+    },
+    migrations: {
+      directory: "./src/db/migrations",
+    },
+    seeds: {
+      directory: "./src/db./seeds",
+    },
+  },
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import app from "./app";
+import db from "./db/knex";
 
 const PORT = process.env.PORT || 5000;
 
+const testDbConnection = async (retries = 5, delay = 2000) => {
+  while (retries) {
+    try {
+      await db.raw("SELECT 1");
+      console.log("Database connected successfully");
+      break;
+    } catch (err) {
+      console.error("Database connection failed:", err);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      if (retries === 0) {
+        console.error("Could not connect to the database. Exiting...");
+        process.exit(1);
+      }
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
+};
+
 app.listen(PORT, () => {
+  // Test database connection
+  testDbConnection();
   console.log(`Server is running on port ${PORT}`);
 });
